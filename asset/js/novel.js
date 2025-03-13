fd5b06a993cff70b0a4771925a1af145f484a743
unknown <541231194@student.smktelkom-pwt.sch.id>
2025-03-13 22:48:07 +0700

    first commit

---
+++ asset/js/novel.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const dropbtn = document.querySelector(".dropbtn");
+    const dropdownContent = document.querySelector(".dropdown-content");
+
+    dropbtn.addEventListener("click", function(event) {
+        event.stopPropagation(); 
+        dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+    });
+
+   
+    document.addEventListener("click", function(event) {
+        if (!dropbtn.contains(event.target) && !dropdownContent.contains(event.target)) {
+            dropdownContent.style.display = "none";
+        }
+    });
+});
+
+
